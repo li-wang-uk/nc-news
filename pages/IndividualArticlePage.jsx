27cac99f86cc1763c2f9c82a24dd899024c5fa37
li-wang-uk <li.wang_@outlook.com>
2023-12-12 15:57:03 +0000

    finish comments list

--- pages/IndividualArticlePage.jsx
+++ pages/IndividualArticlePage.jsx
@@ -1,9 +1,11 @@
 import ArticleContent from "../components/ArticleContent";
+import CommentList from "../components/CommentList";
 
 export default function IndividualArticalPage() {
     return (
         <main>
             <ArticleContent/>
+            <CommentList/>
         </main>
     )
 }
